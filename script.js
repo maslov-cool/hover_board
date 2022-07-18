0e72d318bf233cea4d355260a8d95dda98e5f3e4
alex <106116143+maslov-cool@users.noreply.github.com>
2022-07-18 17:42:46 +0300

    ADD

--- script.js
+++ script.js
@@ -5,28 +5,22 @@ const colors=[]
 for (let i = 0; i < SQUARE_NUMBERS; i++) {
     const square=document.createElement('div')
     square.classList.add('square')
-    square.addEventListener('mouseover',()=>{
-        setColor(square)
-    })
-    square.addEventListener('pointerover',()=>{
-   setColor(square)
-    })
-    square.addEventListener('mouseleave',()=>{
-        removeColor(square)
-    })
-    square.addEventListener('pointerleave',()=>{
-        removeColor(square)
-    })
+    square.addEventListener('mouseover',setColor)
+    square.addEventListener('pointerover',setColor)
+    square.addEventListener('mouseleave',removeColor)
+    square.addEventListener('pointerleave', removeColor)
     board.appendChild(square)
 }
 
-function setColor(element) {
+function setColor(event) {
+    const element=event.target
     const rndColor='#' + (Math.random().toString(16) + '000000').substring(2,8).toUpperCase()
     element.style.backgroundColor=rndColor
     element.style.boxShadow=`0 0 2px ${rndColor},0 0 10px ${rndColor}`
 }
 
-function removeColor(element) {
+function removeColor(event) {
+    const element=event.target
     element.style.backgroundColor='#1d1d1d'
     element.style.boxShadow=`0 0 2px #000`
 }
